dad38c3dcd43a59f333cd6384c5d5673c91a566a
jhosyta13 <117184775+j23591492@users.noreply.github.com>
2024-07-12 09:46:52 -0300

    Add files via upload

---
+++ my proyect/MyDossie.py
@@ -0,0 +1,22 @@
+#inserção de dados...
+print("Digite o seu nome:")
+Myname = star(input())
+print("Digite a sua idade:")
+MyAge = int(input())
+print("Digite a sua altura:")
+Myheight = flot(input())
+
+# Declaração de variáveis...
+Myname = ""
+Myheight = 1.70
+MyAge = 
+Myheight = 90
+MyAge = Parda
+#Exibição do contenido das variáveis...
+print(f"meu nome é{jhoselith}.")
+print(f"minha Altura e de {1.70} metros.")
+print(f"minha idade e {30} Anos.")
+print(f"meu peso e{90}kilos.")
+print(f"minha cor de pele e {branca}")
+print(f"meu cor de cabelo e {preto}")
+print(f"meu telefone e {21967514733}")
